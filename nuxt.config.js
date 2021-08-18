ee7b7a6868ec4096924f1f9dd5be413904ca4abc
Hassan K. Alkhalidi <gruceing@gmail.com>
2021-08-19 00:07:34 +0300

    init

---
+++ nuxt.config.js
@@ -0,0 +1,77 @@
+export default {
+  // Disable server-side rendering: https://go.nuxtjs.dev/ssr-mode
+  ssr: false,
+
+  // Target: https://go.nuxtjs.dev/config-target
+  target: 'static',
+
+  // Global page headers: https://go.nuxtjs.dev/config-head
+  head: {
+    title: 'enabweb',
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { hid: 'description', name: 'description', content: '' },
+      { name: 'format-detection', content: 'telephone=no' }
+    ],
+    link: [
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+    ]
+  },
+
+  // Global CSS: https://go.nuxtjs.dev/config-css
+  css: [
+    '~/assets/css/font.css',
+    '~/assets/css/quick-website.css',
+    '~/assets/css/responsive.css',
+    '~/assets/scss/app.scss',
+    'vuejs-dialog/dist/vuejs-dialog.min.css',
+    'vue-select/dist/vue-select.css',
+  ],
+
+  // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
+  plugins: [
+    {src: '~/plugins/vuejs-dialog', mode: 'client'},
+    {src: '~/plugins/vuejs-select.js', mode: 'client'},
+    {src: '~/plugins/vue-long-click.js', mode: 'client'},
+  ],
+
+  // Auto import components: https://go.nuxtjs.dev/config-components
+  components: true,
+
+  // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
+  buildModules: [
+  ],
+
+  // Modules: https://go.nuxtjs.dev/config-modules
+  modules: [
+    '@nuxtjs/style-resources',
+    // https://go.nuxtjs.dev/bootstrap
+    'bootstrap-vue/nuxt',
+    // https://go.nuxtjs.dev/axios
+    '@nuxtjs/axios',
+    // https://go.nuxtjs.dev/pwa
+    '@nuxtjs/pwa',
+
+  ],
+
+  // Axios module configuration: https://go.nuxtjs.dev/config-axios
+  axios: {},
+
+  // PWA module configuration: https://go.nuxtjs.dev/pwa
+  pwa: {
+    manifest: {
+      lang: 'en'
+    }
+  },
+
+  // Build Configuration: https://go.nuxtjs.dev/config-build
+  build: {
+  },
+
+  styleResources: {
+    scss: [
+        '~/assets/scss/variables.scss',
+    ]
+  }
+}
