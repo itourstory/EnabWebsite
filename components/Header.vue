4112ffedc02af743641f20ba2878897359dff1b1
Hassan K. Alkhalidi <gruceing@gmail.com>
2021-08-19 23:49:28 +0300

    a

--- components/Header.vue
+++ components/Header.vue
@@ -13,15 +13,16 @@
               <nuxt-link to="/" :class="{ 'active' : $nuxt.$route.path === '/'}">الصفحة الرئيسية</nuxt-link>
           </b-nav-item>
           <b-nav-item href="#">
+              <nuxt-link to="/download" :class="{ 'active' : $nuxt.$route.path === '/download'}">تحميل</nuxt-link>
+          </b-nav-item>
+          <b-nav-item href="#">
               <nuxt-link to="/faq" :class="{ 'active' : $nuxt.$route.path === '/faq'}">الأسئلة الشائعة</nuxt-link>
           </b-nav-item>
         </b-navbar-nav>
 
         <!-- Right aligned nav items -->
-        <b-navbar-nav class="mr-auto">
-            <nuxt-link to="/download" class="btn btn-light r-2">
-                تحميل مجاناً
-            </nuxt-link>
+        <b-navbar-nav class="mr-auto text-light">
+            مجاناً
         </b-navbar-nav>
       </b-collapse>
     </b-navbar>
